2ce2d372ae02b0407e1d3b5240599dd1dacd55c2
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-03-12 15:29:59 +0100

    Initial commit

---
+++ src/main/java/com/github/jbence1994/erp/ErpApplication.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jbence1994/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
